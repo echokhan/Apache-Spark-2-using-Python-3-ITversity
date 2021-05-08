81ed98bff75a62f82aaf7e8698bb0c5723418504
Hamza Ehsan Khan <hamzaehsankhan@gmail.com>
2021-05-08 13:02:28 +0500

    Showing how to create generic filtering function by passing lambda function in parameter

--- 7- Manipulating Collections using Map Reduce APIs - Python 3/7-MapReduceDemo.py
+++ 7- Manipulating Collections using Map Reduce APIs - Python 3/7-MapReduceDemo.py
@@ -45,3 +45,34 @@ def itemforid(order_items, order_id):
             order_items_specific_id.append(i)
     return order_items_specific_id
 
+
+#The above functions may have different functionalities, however all involve
+#iterating over each record of the colleciton passed in the parameter and filtering
+#out records according to a particular condition
+#If we are to somehow pass the condition as a parameter, we can create a general-purpose
+def myFilter(c, f):
+    ordersFiltered = []
+    for i in c:
+        if(f(i)):
+            ordersFiltered.append(i)
+    return ordersFiltered
+
+
+ordersFiltered = myFilter(orders, lambda f: f.split(',')[3] == "COMPLETE")
+print("\n\nFiltering COMPLETE data")
+for i in ordersFiltered[:10]:
+    print(i)
+
+print("\n\nFiltering data according to date")
+ordersFiltered = myFilter(orders, lambda f: (f.split(',')[1])[:10] == "2013-07-25")
+for i in ordersFiltered[:10]:
+    print(i)
+
+print("\n\nFiltering data according to order_id")
+ordersFiltered = myFilter(order_items, lambda f: (f.split(',')[1])[:10] == str(2))
+for i in ordersFiltered[:10]:
+    print(i)
+
+####################################################################
+########################Map Function################################
+####################################################################
