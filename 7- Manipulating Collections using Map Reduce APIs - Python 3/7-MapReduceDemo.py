198bbcbe1260a3bf64dce89d273e60c38d583fef
Hamza Ehsan Khan <hamzaehsankhan@gmail.com>
2021-05-08 10:21:31 +0500

    Adding orders and order_items schema, also 7-MapReduceDemo functions added

--- 7- Manipulating Collections using Map Reduce APIs - Python 3/7-MapReduceDemo.py
+++ 7- Manipulating Collections using Map Reduce APIs - Python 3/7-MapReduceDemo.py
@@ -5,16 +5,43 @@ def readData(dataPath):
     dataStr = dataFile.read()
     dataList = dataStr.splitlines()
     return dataList
-print(os.getcwd())
-print(os.path.exists("data\\retail_db\\orders\\part-00000"))
-ordersPath = "data\\retail_db\\orders\\part-00000"
 
+
+ordersPath = "data\\retail_db\\orders\\part-00000"
 orders = readData(ordersPath)
-print(orders[:10])
+#print(orders[:10])
 
 order_itemsPath = "data\\retail_db\\order_items\\part-00000"
 order_items = readData(order_itemsPath)
 print(order_items[:10])
 
+##########################################################
+######Get COMPLETE orders from orders data set############
+##########################################################
+def getcompleteorders(orders):
+    CompleteOrders = []
+    for i in orders:
+        if(i.split(',')[3] == 'COMPLETE'):
+            CompleteOrders.append(i)
+    return CompleteOrders
 
+##########################################################
+######Get orders placed on 2013-07-25#####################
+########################################################## 
+def getdateorders(orders):
+    specificdatelist = []
+    for i in orders:
+        if((i.split(',')[1])[:10] == '2013-07-25'):
+            specificdatelist.append(i)
+    return specificdatelist
+
+##########################################################
+######Get order items for a given order id################
+########################################################## 
+def itemforid(order_items, order_id):
+    order_items_specific_id = []
+    for i in order_items:
+        if(i.split(',')[1] == str(order_id)):
+            order_items_specific_id.append(i)
+    return order_items_specific_id
 
--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 Practice and exercise code for playlist with link as follows:  
 https://www.youtube.com/playlist?list=PLf0swTFhTI8qtIYxVoPOjA2fYzBFiNMue
 
+####################################################
+                    Data
+####################################################
+Dataset: Orders
+Schema: order_id, order_date, order_customer_id, order
+Path = "data\\retail_db\\orders\\part-00000"
+______________________________________________________
 
+Dataset: Order Items
+Schema: order_item_id, order_id, product_id, quantity, subtotal, price
+Path = "data\\retail_db\\orders\\part-00000"
