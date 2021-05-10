0a128ef168cb593cbf5dc500045ac50826d93435
Hamza Ehsan Khan <hamzaehsankhan@gmail.com>
2021-05-10 13:01:05 +0500

    Python revision with some analysis of dataset using pandas library

---
+++ 8- Quick Revision of Python 3/QuickRevisionofPython.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+ordersPath = "data\\retail_db\\orders\\part-00000"
+order_itemsPath = "data\\retail_db\\order_items\\part-00000"
+
+orders = pd.read_csv(ordersPath, names = ["order_id", "order_date", "customer_id", "order_status"])
+order_items = pd.read_csv(order_itemsPath, names = ["item_id", "order_id", "product_id", "quantity", "subtotal", "price"])
+print(orders[:10])
+print(order_items[:10])
