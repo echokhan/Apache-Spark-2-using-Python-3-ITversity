9942b8b78f7cd7604e4ec6ca6f5e5af6a3003ae3
Hamza Ehsan Khan <hamzaehsankhan@gmail.com>
2021-05-18 12:16:34 +0500

    Peforming pyspark filter, map and reduce on RDD read from textfile

--- 9- Apache Spark 2 - Spark Architecture and Execution Modes/Spark Architecture and Execution Modes.py
+++ 9- Apache Spark 2 - Spark Architecture and Execution Modes/Spark Architecture and Execution Modes.py
@@ -10,5 +10,12 @@ conf = SparkConf().setAppName('SparkArchitecture').setMaster('local')
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
 
+#Reading the text file into an RDD, performing filter, map and reduce functions on the RDD
 orderitems = sc.textFile("data\\retail_db\\order_items\\part-00000")
 print(orderitems.take(10))
+orderitemsfiltered = orderitems.filter(lambda k: int(k.split(',')[1]) == 2)
+print(orderitemsfiltered.collect())
+orderitemsMap = orderitemsfiltered.map(lambda k: float(k.split(',')[4]))
+print(orderitemsMap.collect())
+orderitemsReduce = orderitemsMap.reduce(lambda x,y: x + y)
+print(orderitemsReduce)
